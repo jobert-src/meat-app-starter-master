88781cb4b2d2b85fe7074a1c5c41e4459a1c81d5
jobert <jobert@mail.com>
2017-08-25 03:15:37 -0300

    aulas 34-36

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
 })
 
 export class RestaurantsComponent implements OnInit {
